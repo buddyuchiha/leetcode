59f7460e8298a8ed6c9376639c6962e33f987fcb
Buddy <Pro100biv@mail.ru>
2025-04-09 20:36:48 +0400

    the best 217 way

--- array/217_ContainsDuplicate.py
+++ array/217_ContainsDuplicate.py
@@ -1,6 +1,5 @@
 # Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
 def containsDuplicate(nums: list[int]) -> bool:
-    nums_2 = list(set(nums))
-    return len(nums_2) > len(nums)
+    return len(set(nums)) != len(nums)
 
